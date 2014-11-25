cc9829567c16fdf1b71760eaf9e99ef4f5d6eb17
Ben Manning <bmanning@me.com>
2014-11-24 19:56:48 -0800

    add new files

--- test/flatten.js
+++ test/flatten.js
@@ -4,3 +4,12 @@ var expect = require('chai').expect;
 // and returns a new array that is not nested, but contains the same elements
 // as the original array.
 // Then write the method that will make that test pass.
+
+describe('#flatten()', function() {
+	it('loops over nested array and returns unnested array with same elements'), function() {
+		var arr = [1,2[3,4]]
+		var result = flatten(arr)
+    }
+
+    expect(JSON.stringify(result)).to.equal('[1,2,3,4');
+});
--- test/last.js
+++ test/last.js
@@ -3,3 +3,13 @@ var expect = require('chai').expect;
 // Write a test for a method called 'last' that returns the last element
 // of an array.
 // Then write the method that will make that test pass.
+
+describe ('#last()', function() {
+	it('returns the last element of an array', function() {
+		expect(last([3,4,5])).to.equal(5);
+    });
+})last
+
+var last = function(array) {
+	return array[2]
+}
